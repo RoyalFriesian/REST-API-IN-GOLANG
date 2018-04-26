ea87c62a235cfb6b0189638b77f49537e1f7e604
Tarun Bhardwaj <tarun_bhardwaj@hotmail.com>
2018-04-26 12:18:07 +0530

    Create rest.go

---
+++ rest.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+"encoding/json"
+"github.com/gorilla/mux"
+"log"
+"net/http"
+)
+
+// The person Type (more like an object)
+type Person struct {
+    ID        string   `json:"id,omitempty"`
+    Firstname string   `json:"firstname,omitempty"`
+    Lastname  string   `json:"lastname,omitempty"`
+    Address   *Address `json:"address,omitempty"`
+}
+type Address struct {
+    City  string `json:"city,omitempty"`
+    State string `json:"state,omitempty"`
+}
+
+var people []Person
+
+// Display all from the people var
+func GetPeople(w http.ResponseWriter, r *http.Request) {
+    json.NewEncoder(w).Encode(people)
+}
+
+// Display a single data
+func GetPerson(w http.ResponseWriter, r *http.Request) {
+ 
+// It is just a test how to change content-type , You have to set content-type before write header
+    w.Header().Set("Content-Type", "application/json; charset=utf-8")
+    params := mux.Vars(r)
+    for _, item := range people {
+        if item.ID == params["id"] {
+            json.NewEncoder(w).Encode(item)
+
+            return
+        }else{
+            // This is to change the status code
+            w.WriteHeader(406)
+          //  w.Write()
+        }
+    }
+    json.NewEncoder(w).Encode(&Person{})
+}
+
+// create a new item
+func CreatePerson(w http.ResponseWriter, r *http.Request) {
+    params := mux.Vars(r)
+    var person Person
+    _ = json.NewDecoder(r.Body).Decode(&person)
+    person.ID = params["id"]
+    people = append(people, person)
+    json.NewEncoder(w).Encode(people)
+}
+
+// Delete an item
+func DeletePerson(w http.ResponseWriter, r *http.Request) {
+    params := mux.Vars(r)
+    for index, item := range people {
+        if item.ID == params["id"] {
+            people = append(people[:index], people[index+1:]...)
+            break
+        }
+        json.NewEncoder(w).Encode(people)
+    }
+}
+
+// main function to boot up everything
+func main() {
+    router := mux.NewRouter()
+    people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}})
+    people = append(people, Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &Address{City: "City Z", State: "State Y"}})
+    router.HandleFunc("/people", GetPeople).Methods("GET")
+    router.HandleFunc("/people/{id}", GetPerson).Methods("GET")
+    router.HandleFunc("/people/{id}", CreatePerson).Methods("POST")
+    router.HandleFunc("/people/{id}", DeletePerson).Methods("DELETE")
+    router.HandleFunc("/people/{id}", GetPerson).Methods("OTHER")
+    log.Fatal(http.ListenAndServe(":8000", router))
+}
